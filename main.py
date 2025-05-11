02c210544dac936d757c76ac627ac368dea25c5c
Bombombinigusini <katanagacevskaia@mail.ru>
2025-05-11 13:15:16 +0300

    цщгпнзущ шуптт ш4гег щущшуецл млщуа щукъзубьзщцу
    уаж зщаулаущд ьщдздлуаавт двалзывдаку0пахзуДЛЛЬ ЗВАЩМОЩУКПЬ  ОМЗЩУЗШЩЬ ТМУАОЗЩПКОПОЖПТЖЛВАИВТ ТЖЛМЫПЦКЗЩОЦК ОЩАМОЩЗМАЛВС

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class Player(GameSprite):
 
         if keys_pressed[K_DOWN] and self.rect.y < 295:
             self.rect.y  += self.speed
-        
+       
 
 palka_l = Player('pfkri.png',10, 20, 10, 70, 200)
 palka_r = Player('pfkr.png',600, 20, 10, 70, 200)
@@ -41,6 +41,15 @@ win_heigth = 500
 window = display.set_mode(
     (win_width, win_heigth)
 )
+propyshenoP = 0
+propyshenoL = 0
+
+font.init()
+font2 = font.SysFont('Arial', 35)  
+
+text_propyshenoP = font2.render('Пропущено:' + str(propyshenoP), 1, (0,0,0))
+text_propyshenoL = font2.render('Пропущено:' + str(propyshenoL), 1, (0,0,0))
+
 
 clock = time.Clock()
 FPS = 60
@@ -49,12 +58,52 @@ speed_x = 3
 speed_y = 3
 finish = False
 
+
 while game:
     window.fill((200,255,255))
+    
+    
+    if ball.rect.x < 0:
+        ball.rect.x = 250
+        ball.rect.y = 250
+        propyshenoL += 1
+
+    if ball.rect.x > 700:
+        ball.rect.x = 250
+        ball.rect.y = 250
+        propyshenoP += 1  
+    
+    text_propyshenoP = font2.render('Пропущено:' + str(propyshenoP), 1, (0,0,0))
+    window.blit(text_propyshenoP,(450,25))
+
+    text_propyshenoL = font2.render('Пропущено:' + str(propyshenoL), 1, (0,0,0))
+    window.blit(text_propyshenoL,(50,25))
+
     for e in event.get():
         if e.type == QUIT:
             game = False
-        
+    
+    if propyshenoP > 3:
+        text_propyshenoP = font2.render(
+            'YOU LOSE', True, (255,215,0) )
+        text_propyshenoL = font2.render(
+            'YOU WIN', True, (255,215,0) )
+        finish = True
+        window.blit(text_propyshenoP,(450,250))
+        window.blit(text_propyshenoL,(50,250))
+        display.update()     
+
+    if propyshenoL > 3:
+        text_propyshenoL = font2.render(
+            'YOU LOSE', True, (255,215,0) )
+        text_propyshenoP = font2.render(
+            'YOU WIN', True, (255,215,0) )
+
+        finish = True
+        window.blit(text_propyshenoP,(450,250))
+        window.blit(text_propyshenoL,(50,250))
+        display.update()     
+
     if finish != True:
         ball.rect.x += speed_x
         ball.rect.y += speed_y
@@ -64,13 +113,18 @@ while game:
 
         if sprite.collide_rect(palka_l, ball)  or sprite.collide_rect(palka_r, ball):
             speed_x *= -1
+    keys_pressed = key.get_pressed()
+    if keys_pressed[K_RETURN]:
+        propyshenoL = 0
+        propyshenoP = 0
+        finish = False
 
 
-        palka_l.update_l()
-        palka_l.reset()
-        palka_r.update_r()
-        palka_r.reset()
-        ball.update()
-        ball. reset()
+    palka_l.update_l()
+    palka_l.reset()
+    palka_r.update_r()
+    palka_r.reset()
+    ball.update()
+    ball. reset()
     display.update()
     clock.tick(FPS)
